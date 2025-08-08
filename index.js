fc4d4dc0b3613921fd9785993781fd27ca680696
Durgesh-sharma9 <162336598+Durgesh-sharma9@users.noreply.github.com>
2025-08-08 17:31:06 +0530

    second commit

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000;
-
+const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/login', (req, res) => {
-  res.send('Hello dev')
-})
-
-
-app.listen(process.env.port, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
